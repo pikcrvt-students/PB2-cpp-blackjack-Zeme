46ff106a09192a4cfcb5a988841ae7831f7ff8a4
21DPGZeme <103510318+21DPGZeme@users.noreply.github.com>
2022-10-31 10:09:29 +0200

    Update blackjack.cpp
    Salaboju "ace" kārts vērtību, kad izdala pirmās kārtis

--- blackjack.cpp
+++ blackjack.cpp
@@ -44,166 +44,182 @@ string input;
 
 // Function definition
 void addPcard() {
-  Phand1[Pcount] = cards[Ccount];
-  Phand2[Pcount] = values[Ccount];
-  Ccount++;
-  Pcount++;
+    Phand1[Pcount] = cards[Ccount];
+    Phand2[Pcount] = values[Ccount];
+    Ccount++;
+    Pcount++;
 }
 void addDcard() {
-  Dhand1[Dcount] = cards[Ccount];
-  Dhand2[Dcount] = values[Ccount];
-  Ccount++;
-  Dcount++;
+    Dhand1[Dcount] = cards[Ccount];
+    Dhand2[Dcount] = values[Ccount];
+    Ccount++;
+    Dcount++;
 }
 int Phandsum() {
-  int sum = 0;
-  for (int i = 0; i < Pcount; i++) {
-    sum+= Phand2[i];
-  }
-  return sum;
+    int sum = 0;
+    for (int i = 0; i < Pcount; i++) {
+        sum+= Phand2[i];
+    }
+    return sum;
 }
 int Dhandsum() {
-  int sum = 0;
-  for (int i = 0; i < Dcount; i++) {
-    sum+= Dhand2[i];
-  }
-  return sum;
+    int sum = 0;
+    for (int i = 0; i < Dcount; i++) {
+        sum+= Dhand2[i];
+    }
+    return sum;
 }
 void showPhand() {
-  cout << "Your hand (" << Phandsum() << "):\n";
-  for (int i = 0; i < Pcount; i++) {
-    cout << "- " << Phand1[i] << "\n";
-  }
-  cout << "\n";
+    cout << "Your hand (" << Phandsum() << "):\n";
+    for (int i = 0; i < Pcount; i++) {
+        cout << "- " << Phand1[i] << "\n";
+    }
+    cout << "\n";
 }
 void showDhand(string x) {
-  system("CLS");
-  if (x == "show") {
-    cout << "Dealer's hand (" << Dhandsum() << "):\n";
-    for (int i = 0; i < Dcount; i++) {
-      cout << "- " << Dhand1[i] << "\n";
+    system("CLS");
+    if (x == "show") {
+        cout << "Dealer's hand (" << Dhandsum() << "):\n";
+        for (int i = 0; i < Dcount; i++) {
+            cout << "- " << Dhand1[i] << "\n";
+        }
     }
-  }
-  else {
-    cout << "Dealer's hand (" << Dhandsum() - Dhand2[0] << "):\n";
-    cout << "- hidden card\n";
-    for (int i = 1; i < Dcount; i++) {
-      cout << "- " << Dhand1[i] << "\n";
+    else {
+        cout << "Dealer's hand (" << Dhandsum() - Dhand2[0] << "):\n";
+        cout << "- hidden card\n";
+        for (int i = 1; i < Dcount; i++) {
+            cout << "- " << Dhand1[i] << "\n";
+        }
     }
-  }
-  cout << "\n";
+    cout << "\n";
 }
 
 
 // Main game loop
 int main() {
-  SetConsoleOutputCP(65001);
+    SetConsoleOutputCP(65001);
 
-  // Deck shuffle
-  srand(time(0));
-  for (int i = 0; i < 52 ; i++) {
-    int r = i + (rand() % (52 - i));
-    swap(cards[i], cards[r]);
-    swap(values[i], values[r]);
-  }
+    // Deck shuffle
+    srand(time(0));
+    for (int i = 0; i < 52 ; i++) {
+        int r = i + (rand() % (52 - i));
+        swap(cards[i], cards[r]);
+        swap(values[i], values[r]);
+    }
 
-  // First card dealing
-  for (int i = 0; i < 2; i++) {
-    addPcard();
-    addDcard();  
-  }
-  showDhand("");
-  showPhand();
-  if (Dhand2[1] == 10 || Dhand2[1] == 11) {
-    system("pause");
-  }
-  
-  // The rest of the game
-  if (Dhandsum() == 21) {
-    showDhand("show");
-    showPhand();
-    if (Phandsum() == 21) {
-      cout << "You and the dealer both have blackjack! It's a tie!\n";
-      system("pause");
-    }
-    else {
-      cout << "Dealer has blackjack! Dealer wins!\n";
-      system("pause");
-    }
-  }
-  else if (Phandsum() == 21) {
-    showDhand("show");
-    showPhand();
-    cout << "You have blackjack! You win!\n";
-    system("pause");
-  }
-  else {
-    showDhand("");
-    showPhand();
-    while (Pcount < 5 && Phandsum() < 21) {
-      cout << "Input H to hit or S to stand:\n";
-      cin >> input;
-      cout << "\n";
-      if (input == "H" || input == "h") {
+    // First card dealing
+    for (int i = 0; i < 2; i++) {
         addPcard();
-        if (Phandsum() > 21) {
-          for (int i = 0; i < 5; i++) {
-            if (Phand2[i] == 11) {
-              Phand2[i] = 1;
-              if (Phandsum() < 21) {
-                break;
-              }
-            }
-          }
-        }
-        showDhand("");
-        showPhand();
-      }
-      if (input == "S" || input == "s") {
-        break;
-      }
+        addDcard();
     }
     if (Phandsum() > 21) {
-      showDhand("show");
-      showPhand();
-      cout << "Bust! You lose!\n";
-      system("pause");
-    }
-    else {
-      showDhand("show");
-      showPhand();
-      while (Dhandsum() < 17 && Dcount < 5) {
-        system("pause");
-        addDcard();
-        if (Dhandsum() > 21) {
-          for (int i = 0; i < 5; i++) {
-            if (Dhand2[i] == 11) {
-              Dhand2[i] = 1;
-              if (Dhandsum() < 21) {
+        for (int i = 0; i < 2; i++) {
+            if (Phand2[i] == 11) {
+                Phand2[i] = 1;
                 break;
-              }
             }
-          }
         }
+    }
+    if (Dhandsum() > 21) {
+        for (int i = 0; i < 2; i++) {
+            if (Dhand2[i] == 11) {
+                Dhand2[i] = 1;
+                break;
+            }
+        }
+    }
+    showDhand("");
+    showPhand();
+    if (Dhand2[1] == 10 || Dhand2[1] == 11) {
+        system("pause");
+    }
+
+    // The rest of the game
+    if (Dhandsum() == 21) {
         showDhand("show");
         showPhand();
-      }
-      if (Dhandsum() > 21) {
-        cout << "The dealer has bust! You win!\n";
-        system("pause");
-      }
-      else if (Phandsum() > Dhandsum()) {
-        cout << "You Won!\n";
-        system("pause");
-      }
-      else if (Phandsum() == Dhandsum()) {
-        cout << "Draw!\n";
-        system("pause");
-      }
-      else {
-        cout << "You lose!\n";
-        system("pause");
-      }
+        if (Phandsum() == 21) {
+            cout << "You and the dealer both have blackjack! It's a tie!\n";
+            system("pause");
+        }
+        else {
+            cout << "Dealer has blackjack! Dealer wins!\n";
+            system("pause");
+        }
     }
-  }
+    else if (Phandsum() == 21) {
+        showDhand("show");
+        showPhand();
+        cout << "You have blackjack! You win!\n";
+        system("pause");
+    }
+    else {
+        showDhand("");
+        showPhand();
+        while (Pcount < 5 && Phandsum() < 21) {
+            cout << "Input H to hit or S to stand:\n";
+            cin >> input;
+            cout << "\n";
+            if (input == "H" || input == "h") {
+                addPcard();
+                if (Phandsum() > 21) {
+                    for (int i = 0; i < 5; i++) {
+                        if (Phand2[i] == 11) {
+                            Phand2[i] = 1;
+                            if (Phandsum() < 21) {
+                                break;
+                            }
+                        }
+                    }
+                }
+                showDhand("");
+                showPhand();
+            }
+            if (input == "S" || input == "s") {
+                break;
+            }
+        }
+        if (Phandsum() > 21) {
+            showDhand("show");
+            showPhand();
+            cout << "Bust! You lose!\n";
+            system("pause");
+        }
+        else {
+            showDhand("show");
+            showPhand();
+            while (Dhandsum() < 17 && Dcount < 5) {
+                system("pause");
+                addDcard();
+                if (Dhandsum() > 21) {
+                    for (int i = 0; i < 5; i++) {
+                        if (Dhand2[i] == 11) {
+                            Dhand2[i] = 1;
+                            if (Dhandsum() < 21) {
+                                break;
+                            }
+                        }
+                    }
+                }
+                showDhand("show");
+                showPhand();
+            }
+            if (Dhandsum() > 21) {
+                cout << "The dealer has bust! You win!\n";
+                system("pause");
+            }
+            else if (Phandsum() > Dhandsum()) {
+                cout << "You Won!\n";
+                system("pause");
+            }
+            else if (Phandsum() == Dhandsum()) {
+                cout << "Draw!\n";
+                system("pause");
+            }
+            else {
+                cout << "You lose!\n";
+                system("pause");
+            }
+        }
+    }
 }
